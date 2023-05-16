be8512b8af7cbea6f3602da73c082bfc54eed28e
Darkissons <inaciormgalvao@outlook.com>
2023-05-16 20:26:19 -0300

    all fixes on server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Datastore = require("nedb");
 const ejs = require("ejs");
 const path = require("path");
 const fs = require("fs");
+const { pathToRegexp } = require("path-to-regexp");
 
 const app = express();
-const db = new Datastore({ filename: "data.db", autoload: true });
 
 const routes = [
   { path: "/", component: "pages/index" },
@@ -15,35 +14,68 @@ const routes = [
   { path: "/perfil/:id", component: "pages/profile" },
 ];
 
+const routeMatchers = routes.map((route) => {
+  const keys = [];
+  const pattern = pathToRegexp(route.path, keys);
+  return { route, pattern, keys };
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  const route = routes.find((r) => {
-    const pattern = new RegExp(`^${r.path.replace(/:[^\s/]+/g, "([\\w-]+)")}$`);
-    return pattern.test(req.url);
-  });
+  let match = null;
+  for (const { route, pattern, keys } of routeMatchers) {
+    const result = pattern.exec(req.path);
+    if (result) {
+      match = { route, keys, params: {} };
+      for (let i = 1; i < result.length; i++) {
+        const key = keys[i - 1];
+        const value = result[i];
+        match.params[key.name] = value;
+      }
+      break;
+    }
+  }
 
-  if (route) {
+  if (match) {
+    const { route, params } = match;
     const componentPath = path.join(
       __dirname,
       "views",
       route.component + ".ejs"
     );
-    const params = { ...req.params };
-    const queryParams = { ...req.query };
+    const queryParams = req.query;
     const data = { ...params, ...queryParams };
+
+    const isFileRequest = req.url.includes(".");
+    const newData = isFileRequest ? {} : data;
+
     fs.readFile(componentPath, "utf8", (err, content) => {
       if (err) {
         console.error(err);
         res.status(500).send("<h1>Erro interno do servidor</h1>");
       } else {
-        res.render("main", {
-          content,
-          data,
-        });
+        const renderedContent = ejs.render(content, data);
+        const mainTemplatePath = path.join(__dirname, "views", "main.ejs");
+
+        ejs.renderFile(
+          mainTemplatePath,
+          {
+            content: renderedContent,
+            data: newData,
+          },
+          (err, mainContent) => {
+            if (err) {
+              console.error(err);
+              res.status(500).send("<h1>Erro interno do servidor</h1>");
+            } else {
+              res.send(mainContent);
+            }
+          }
+        );
       }
     });
   } else {
